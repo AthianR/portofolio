c3db3a4bd32ccfda1a49321f3556c4fe758326b2
AthianR <iyanthubet@gmail.com>
2023-09-17 18:54:33 +0700

    perbaikan ketiga

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.app')
 @section('content')
-<div class="project">
-    <h1>Project yang telah dikerjakan</h1>
-</div>
+    <div class="container mx-auto py-12 mt-10">
+        <div class="crd bg-gray-900 p-12 rounded-lg shadow-lg">
+            <h1 class="text-3xl font-semibold text-gray-50 mb-4">About Us</h1>
+            <p class="text-gray-50 leading-relaxed">
+                Web ini berisikan portofolio Athian Rizki, yang dibuat dengan Framework Laravel dan Tailwind CSS dengan
+                ekstensi DaisyUi. Selain itu web ini juga di style custom dengan menggunakan file css dan juga javascript.
+                Versi laravel yang saya gunakan yaitu versi 8.0.
+            </p>
+            <div class="mt-4">
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-2">
+                    <a href="{{ url('/') }}">
+                    <div class="bg-gray-950 hover:scale-110 transition duration-300 delay-150 p-4 rounded-lg shadow-lg">
+                        <img src="../images/athian.jpg" alt="Foto Anggota Tim"
+                            class="w-20 h-20 rounded-full mx-auto mb-2">
+                        <h3 class="text-lg font-semibold text-gray-50">Athian Rizki</h3>
+                        <p class="text-sm text-gray-50">Fullstack Laravel</p>
+                    </div>
+                </a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/profile/project.blade.php
+++ resources/views/profile/project.blade.php
@@ -1,6 +1,46 @@
 @extends('layouts.app')
 @section('content')
-    <div class="project">
-        <h1>Project yang telah dikerjakan</h1>
+    <div class="container mx-auto py-12 mt-10">
+        <div class="pr bg-gray-950 p-6 rounded-lg shadow-lg">
+            <h1 class="text-3xl font-semibold text-gray-50 mb-4">Project</h1>
+            <p class="text-gray-50 leading-relaxed">
+                Terdapat 3 project yang pernah saya kerjakan dengan menggunakan Framework Laravel. Yaitu project Periksalab
+                yang saya kerjakan pada saat Magang, kemudian project SDN Leuwilaja III merupakan project teman yang saya
+                kerjakan untuk kebutuhan penelitian, lalu project Mhazel project yang berisikan web seperti onlineshop untuk
+                memenuhi tugas perkuliahan.
+            </p>
+            <p class="text-gray-50 leading-relaxed mt-2">
+                Berikut dokumentasi projectnya :
+            </p>
+            <div class="card mt-4">
+                <h2 class="content text-xl font-semibold text-gray-50">My Project</h2>
+                <div class="body grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-2">
+                    <!-- Anggota Tim 1 -->
+                    <div class="body-content bg-gray-800 hover:scale-110 transition duration-300 delay-150 p-4 rounded-lg shadow-lg">
+                        <a href="#">
+                            <img src="../images/periksa.png" alt="Foto Anggota Tim" class="w-100 h-100 mx-auto mb-2 rounded-lg">
+                        </a>
+                        <h3 class="text-lg font-semibold text-gray-50">Periksalab.id</h3>
+                        <p class="text-sm text-gray-50">Project Magang PT. Solusi Tiga Selaras</p>
+                    </div>
+                    <!-- Anggota Tim 2 -->
+                    <div class="body-content bg-gray-800 hover:scale-110 transition duration-300 delay-150 p-4 rounded-lg shadow-lg">
+                        <a href="https://github.com/AthianR/sdnleuwilaja">
+                            <img src="../images/sd.png" alt="Foto Anggota Tim" class="w-100 h-100 mx-auto mb-2 rounded-lg">
+                        </a>
+                        <h3 class="text-lg font-semibold text-gray-50">sdnleuwilajaIII</h3>
+                        <p class="text-sm text-gray-50">Project penelitian teman</p>
+                    </div>
+                    <!-- Anggota Tim 3 -->
+                    <div class="body-content bg-gray-800 hover:scale-110 transition duration-300 delay-150 p-4 rounded-lg shadow-lg">
+                        <a href="https://github.com/AthianR/mhazel">
+                            <img src="../images/mhazel.png" alt="Foto Anggota Tim" class="w-100 h-100 mx-auto mb-2 rounded-lg">
+                        </a>
+                        <h3 class="text-lg font-semibold text-gray-50">Mhazel</h3>
+                        <p class="text-sm text-gray-50">Project Onlineshop</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
